580c2cc0c749b65a0a7110d45c9a888c2f1fee4e
shindonghwi <ehdgnl8940@naver.com>
2022-10-18 16:01:12 +0900

    fix: 가이드 화면 레이아웃 조정

--- app/src/main/java/orot/apps/smartcounselor/presentation/guide/GuideScreen.kt
+++ app/src/main/java/orot/apps/smartcounselor/presentation/guide/GuideScreen.kt
@@ -4,29 +4,19 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import orot.apps.resources.Gray10
-import orot.apps.resources.Gray20
 import orot.apps.smartcounselor.presentation.app_style.Display1
-import orot.apps.smartcounselor.presentation.app_style.Pretendard
 
 @ExperimentalAnimationApi
 @Composable
@@ -54,24 +44,6 @@ private fun GuideContent(modifier: Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//            val currentText = guideViewModel.currentRenderText.collectAsState().value
-
-//    AnimatedContent(
-//        targetState = currentText,
-//        transitionSpec = {
-//            addAnimation().using(
-//                SizeTransform(clip = true)
-//            )
-//        },
-//        contentAlignment = Alignment.Center
-//    ) { content ->
-//        Text(
-//            content,
-//            style = MaterialTheme.typography.Display2,
-//            color = Primary,
-//            textAlign = TextAlign.Center
-//        )
-//    }
         Text(
             "Mago Healthcare",
             style = MaterialTheme.typography.Display1,
