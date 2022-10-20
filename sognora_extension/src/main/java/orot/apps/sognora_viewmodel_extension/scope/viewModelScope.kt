965c7385594fdeeed929a6123031b974d1199f57
shindonghwi <ehdgnl8940@naver.com>
2022-10-20 11:29:57 +0900

    style: 코드정렬

--- sognora_extension/src/main/java/orot/apps/sognora_viewmodel_extension/scope/viewModelScope.kt
+++ sognora_extension/src/main/java/orot/apps/sognora_viewmodel_extension/scope/viewModelScope.kt
@@ -7,19 +7,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 inline fun ViewModel.onMain(
-        crossinline body: suspend CoroutineScope.() -> Unit
-    ) = viewModelScope.launch {
-        body(this)
-    }
+    crossinline body: suspend CoroutineScope.() -> Unit
+) = viewModelScope.launch {
+    body(this)
+}
 
-    inline fun ViewModel.onIO(
-        crossinline body: suspend CoroutineScope.() -> Unit
-    ) = viewModelScope.launch(Dispatchers.IO) {
-        body(this)
-    }
+inline fun ViewModel.onIO(
+    crossinline body: suspend CoroutineScope.() -> Unit
+) = viewModelScope.launch(Dispatchers.IO) {
+    body(this)
+}
 
-    inline fun ViewModel.onDefault(
-        crossinline body: suspend CoroutineScope.() -> Unit
-    ) = viewModelScope.launch(Dispatchers.Default) {
-        body(this)
-    }
+inline fun ViewModel.onDefault(
+    crossinline body: suspend CoroutineScope.() -> Unit
+) = viewModelScope.launch(Dispatchers.Default) {
+    body(this)
+}
