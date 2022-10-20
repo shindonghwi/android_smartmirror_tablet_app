2a2a30cff0565d6b4cb723b339597150ed2babd0
shindonghwi <ehdgnl8940@naver.com>
2022-10-20 19:33:10 +0900

    feat: 채팅화면 제작완료

--- app/src/main/java/orot/apps/smartcounselor/presentation/chat_list/ChatListScreen.kt
+++ app/src/main/java/orot/apps/smartcounselor/presentation/chat_list/ChatListScreen.kt
@@ -1,9 +1,94 @@
 package orot.apps.smartcounselor.presentation.chat_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun ChatListScreen(navController: NavHostController) {
+fun ChatListScreen(
+    chatListViewModel: ChatListViewModel = hiltViewModel()
+) {
+    LazyColumn(
+        modifier = Modifier.padding(top = 40.dp),
+        state = rememberLazyListState(),
+        reverseLayout = false,
+        contentPadding = PaddingValues(bottom = 20.dp)
+    ) {
+        items(
+            count = chatListViewModel.chatDataList.size
+        ) { idx ->
+            ChatContentTypeHolder(chatListViewModel.chatDataList[idx])
+        }
+    }
+}
 
+
+@Composable
+private fun ChatContentTypeHolder(chatData: ChatData) {
+
+    when (chatData.isUser) {
+        false -> {
+            AiTextContent(chatData.msg)
+        }
+        true -> {
+            UserTextContent(chatData.msg)
+        }
+    }
+}
+
+@Composable
+private fun UserTextContent(msg: String) {
+    val config = LocalConfiguration.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 40.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.End
+    ) {
+        Text(
+            modifier = Modifier
+                .widthIn(max = config.screenWidthDp.dp * 0.72f)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.White)
+                .padding(vertical = 14.dp, horizontal = 12.dp),
+            text = msg,
+            style = MaterialTheme.typography.h2,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+private fun AiTextContent(msg: String) {
+    val config = LocalConfiguration.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 40.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .widthIn(max = config.screenWidthDp.dp * 0.72f)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color(0xFFE16145))
+                .padding(vertical = 14.dp, horizontal = 12.dp),
+            text = msg,
+            style = MaterialTheme.typography.h2,
+            color = Color.White
+        )
+    }
 }
