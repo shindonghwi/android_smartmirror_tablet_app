3416dff06d7f64212503e29f3100efa1c219512e
shindonghwi <ehdgnl8940@naver.com>
2022-10-24 14:30:55 +0900

    fix: viewmodel extension 파라미터 변경

--- sognora_extension/src/main/java/orot/apps/sognora_viewmodel_extension/getViewModel.kt
+++ sognora_extension/src/main/java/orot/apps/sognora_viewmodel_extension/getViewModel.kt
@@ -1,6 +1,7 @@
 package orot.apps.sognora_viewmodel_extension
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
@@ -10,12 +11,22 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 val vms = ViewModelStore()
 
 @Composable
-inline fun <reified type : ViewModel>getViewModel(key: String, vm: type) =
-    viewModel<type>(
-        key = key,
+inline fun <reified type : ViewModel> getViewModel(
+    vm: type,
+    optionKey: Int? = 0,
+): type {
+
+    val vmName = vm.javaClass.simpleName
+    val keys = remember { LinkedHashSet<String>() }
+    keys.add("$vmName$optionKey")
+    val findKey = keys.find { it == "$vmName$optionKey" }
+
+    return viewModel(
+        key = findKey,
         viewModelStoreOwner = { vms },
         factory = object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
                 return vm as T
             }
         })
+}
