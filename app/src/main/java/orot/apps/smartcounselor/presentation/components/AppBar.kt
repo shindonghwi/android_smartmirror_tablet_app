8fd06f63d97d5902dd20711de9d6692e360252c1
shindonghwi <ehdgnl8940@naver.com>
2022-10-31 17:24:12 +0900

    fix: appbar 시간표시 삭제

--- app/src/main/java/orot/apps/smartcounselor/presentation/components/AppBar.kt
+++ app/src/main/java/orot/apps/smartcounselor/presentation/components/AppBar.kt
@@ -7,18 +7,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import orot.apps.smartcounselor.BottomMenu
@@ -27,8 +21,6 @@ import orot.apps.smartcounselor.MainViewModel
 import orot.apps.smartcounselor.R
 import orot.apps.smartcounselor.Screens
 import orot.apps.smartcounselor.presentation.app_style.Gray20
-import orot.apps.smartcounselor.presentation.app_style.Pretendard
-import orot.apps.smartcounselor.presentation.app_style.White
 import orot.apps.sognora_viewmodel_extension.getViewModel
 
 @Composable
@@ -145,58 +137,8 @@ fun MagoAppBar(
                         contentScale = ContentScale.FillWidth
                     )
                     Spacer(modifier = Modifier.weight(0.33f))
-
-                    TimeContent()
                 }
             }
         }
     }
-}
-
-@Composable
-private fun TimeContent(
-    mainViewModel: MainViewModel = hiltViewModel()
-) {
-    mainViewModel.currentTime.collectAsState().value.also { currentTime ->
-        val date = currentTime.split("-")[0]
-        val hour = currentTime.split("-")[1].split(":")[0].toInt()
-        val minute = currentTime.split("-")[1].split(":")[1].toInt()
-        val yoil = currentTime.split("-")[2]
-
-        val annotatedString = buildAnnotatedString {
-            withStyle(
-                style = SpanStyle(
-                    color = White,
-                    fontFamily = Pretendard,
-                    fontWeight = FontWeight.W600,
-                    fontSize = 40.sp,
-                )
-            ) {
-                append("${String.format("%02d", hour)}:${String.format("%02d", minute)}")
-            }
-            withStyle(
-                style = SpanStyle(
-                    color = White,
-                    fontFamily = Pretendard,
-                    fontWeight = FontWeight.W600,
-                    fontSize = 26.sp,
-                )
-            ) {
-                append(hour.takeIf { it >= 12 }?.run { "PM" } ?: "AM")
-            }
-        }
-
-        Column(horizontalAlignment = Alignment.End) {
-            Row {
-                Text(text = annotatedString)
-            }
-            Text(
-                text = "$date($yoil)",
-                style = MaterialTheme.typography.subtitle1,
-                color = White,
-            )
-        }
-    }
-
-
 }
